a71493d441fabc7b16f115ac2a02bb3c5a6d0cab
dagmara <dagmara.kacprzak@gmail.com>
2019-03-27 17:07:46 +0100

    html template, posts

--- index.php
+++ index.php
@@ -8,32 +8,85 @@
 
 ?>
 
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PHP CMS</title>
-    <link rel="stylesheet" href="assets/style.css">
-</head>
-<body>
-    <div class="container">
-        <a href="index.php" id="logo">CMS</a>
+<?php include('header.php'); ?>
 
-        <ol>
-            <?php foreach ($articles as $article) { ?>
-                <li>
-                    <a href="article.php?id=<?php echo $article['article_id']; ?>">
-                        <?php echo $article['article_title']; ?>
-                    </a>
-                    - <small>
-                        posted <?php echo date('l jS', $article['article_timestamp']); ?>
-                    </small>
-                </li>
-            <?php } ?>
-        </ol>
-        <br>
-        <small><a href="admin">admin</a></small>
+  <!-- Main Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto"> 
+      <?php foreach ($articles as $article) { ?>    
+        <div class="post-preview">
+            <a href="article.php?id=<?php echo $article['article_id']; ?>">
+                <h2 class="post-title">
+                    <?php echo $article['article_title']; ?>
+                </h2>         
+            
+            <h3 class="post-subtitle">
+              Problems look mighty small from 150 miles up - TO DO!!!!!
+            </h3>
+            </a>
+          <p class="post-meta">Posted by
+            <a href="#">Start Bootstrap</a>
+            on <?php echo date('l jS', $article['article_timestamp']); ?></p>
+        </div>
+        <hr>
+      <?php } ?>
+
+        <!-- Pager -->
+        <div class="clearfix">
+          <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+        </div>
+      </div>
     </div>
+  </div>
+
+  <hr>
+
+  <!-- Footer -->
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <ul class="list-inline text-center">
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-github fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+          </ul>
+          <p class="copyright text-muted">Copyright &copy; Your Website 2019</p>
+          <small><a href="admin">admin</a></small>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.min.js"></script>
+
 </body>
+
 </html>
